a052c6e6ee64f6629339612a646e1c1f0ac66513
Ariel Altamirano <arielaltamirano@softwareevolutivo.com.ec>
2022-06-30 19:31:13 -0500

    Solved exercise

--- src/rumour.js
+++ src/rumour.js
@@ -1,5 +1,15 @@
 export const rumour = (a, b) => {
-    return 0
+  var cont = 0;
+  while (a != b) {
+    if (a > b) {
+      a = parseInt(a / 2);
+    } else {
+      b = parseInt(b / 2);
+    }
+    cont++;
+  }
+
+  return cont;
 };
-  
+
 export default { rumour };
--- test/rumour.test.js
+++ test/rumour.test.js
@@ -1,6 +1,38 @@
 import { rumour }  from '../src/rumour';
 
-test('same node', () => {
+test('should return zero steps when start and end node is the same', () => {
     expect(rumour(1, 1)).toBe(0);
+});
+
+test('should return one step when start node is 1 and end node is 2', () => {
+    expect(rumour(1, 2)).toBe(1);
+});
+
+test('should return two step when start node is 1 and end node is 4', () => {
+    expect(rumour(1, 4)).toBe(2);
+});
+
+test('should return two step when start node is 1 and end node is 5', () => {
+    expect(rumour(1, 5)).toBe(2);
+});
+
+test('should return three step when start node is 1 and end node is 8', () => {
+    expect(rumour(1, 8)).toBe(3);
+});
+
+test('should return three step when start node is 8 and end node is 1', () => {
+    expect(rumour(8, 1)).toBe(3);
+});
+
+test('should return five step when start node is 15 and end node is 5', () => {
+    expect(rumour(15, 5)).toBe(5);
+});
+
+test('should return six step when start node is 8 and end node is 13', () => {
+    expect(rumour(8, 13)).toBe(6);
+});
+
+test('should return five step when start node is 12 and end node is 5', () => {
+    expect(rumour(12, 5)).toBe(5);
 });
 
